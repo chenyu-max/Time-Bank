10537ea56b041c7975cc81b77087fdcbc2b5c53a
chenyu-max <2497191408@qq.com>
2021-07-17 20:36:11 +0800

    登录、注册、找回密码页面-first commit

--- src/router/index.js
+++ src/router/index.js
@@ -40,4 +40,20 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  // 如果想要进入其他的路由，会进行判断
+  if (to.path !== '/login' && to.path !== '/findbackpwd' && to.path !== '/register') {
+    // 从非登录页面进入 系统内部页面 开始进行判断
+    // 或者从非登录页面 改变路由的方式进入系统内部页面 进行判断
+    if (from.path !== '/login') {
+      // 暂时先这么写，之后在 vuex 中 如果用户登录，就会获取一个 appkey 如果有appkey 那么就...
+      return next('/login');
+    }
+    if (from.path === '/login') {
+      // 从登录页面进入系统页面，进行数据填写，并进入系统
+    }
+  }
+  return next();
+});
+
 export default router;
