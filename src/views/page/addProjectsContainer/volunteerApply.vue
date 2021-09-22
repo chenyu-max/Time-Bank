eb7ead882c47eaeb4e609e94c0cd1812af959532
chenyu-max <2497191408@qq.com>
2021-09-22 21:58:52 +0800

    回校动工——第9天

---
+++ src/views/page/addProjectsContainer/volunteerApply.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="volunteer-apply-container">
+    志愿者申请
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'volunteerApply',
+};
+</script>
+
+<style scoped>
+
+</style>
