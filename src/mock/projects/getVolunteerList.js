f6578e8df9381050975069e1eddc1c73c90b5828
chenyu-max <2497191408@qq.com>
2021-09-23 22:59:00 +0800

    回校动工——第9天

---
+++ src/mock/projects/getVolunteerList.js
@@ -0,0 +1,16 @@
+import Mock from 'mockjs';
+
+Mock.mock('/api/volunteerlist', 'get', {
+  code: 0,
+  msg: '',
+  'data|13-33': [
+    {
+      userId: '@id',
+      userName: '@cname',
+      'level|1-7': 0,
+      projectId: '@id',
+      projectName: '@cword(3,5)',
+      time: '@datetime("yyyy-MM-dd HH:mm:ss")',
+    },
+  ],
+});
